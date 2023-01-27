7fd38ec2ad5fba9c42a43963c63e5bc9eecef592
Oleg Mozharovskyi <Oleg_Mozharovskyi@epam.com>
2023-01-27 22:26:49 +0100

    FETCHER: add draft sql class

--- promua_fetcher/order_fetcher.py
+++ promua_fetcher/order_fetcher.py
@@ -1,7 +1,13 @@
 import json
 import http.client
 from config import *
+import mysql.connector
+from mysql.connector import errorcode
 import pprint
+
+
+class HTTPError(Exception):
+    pass
 
 
 class PromuaAPIClient(object):
@@ -20,8 +26,8 @@ class PromuaAPIClient(object):
             body = json.dumps(body)
         connection.request(method, url, body=body, headers=headers)
         response = connection.getresponse()
-        # if response.status != 200:
-        #     raise HTTPError('{}: {}'.format(response.status, response.reason))
+        if response.status != 200:
+            raise HTTPError(f'{response.status}: {response.reason}')
         response_data = response.read()
         return json.loads(response_data.decode())
 
@@ -36,9 +42,42 @@ class PromuaAPIClient(object):
         return self.make_request(method, url)
 
 
+class MySQLClient(object):
+    def __init__(self, server, databasename, user, password):
+        self.context = None
+        self.server = server
+        self.databasename = databasename
+        self.user = user
+        self.password = password
+
+    def connect_db(self):
+        try:
+            self.context = mysql.connector.connect(
+                user=self.user,
+                password=self.password,
+                host=self.server,
+                database=self.databasename,
+                use_pure=False
+            )
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Wrong username or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+
+    def disconnect_db(self):
+        self.context.close()
+
+
 def main():
     api_client = PromuaAPIClient(PROM_KEY, PROM_HOST)
-    pprint.pprint(api_client.get_order_list())
+    # pprint.pprint(api_client.get_order_list())
+    # pprint.pprint(api_client.get_orders_status_list())
+    # orders_list = api_client.get_order_list()
+    # for order in orders_list['orders']:
+    #     pprint.pprint(order['status_name'])
 
 
 if __name__ == '__main__':
