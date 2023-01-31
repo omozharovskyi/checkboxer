400e95b1a6bbdb15d5329bc66aaa132f70a1ae7a
Oleg Mozharovskyi <Oleg_Mozharovskyi@epam.com>
2023-01-31 15:17:06 +0100

    UNITTEST_FETCHER: add TestArgParser suite

---
+++ unit_tests/test_order_fetcher.py
@@ -0,0 +1,23 @@
+import unittest
+import sys
+from promua_fetcher.order_fetcher import init_parameters
+from promua_fetcher.order_fetcher import MySQLClient
+from promua_fetcher.order_fetcher import PromuaAPIClient
+
+
+class TestArgParser(unittest.TestCase):
+    """Suite for testing command line arguments parser"""
+
+    def test_Version(self):
+        """Checking '--version' argument"""
+        sys.argv[1:] = ['--version']
+        with self.assertRaises(SystemExit, msg="'--version' parameter parsed in wrong way"):
+            init_parameters()
+
+    def test_Mode(self):
+        """Checking parsing URL parameter"""
+        sys.argv[1:] = ['fetch']
+        options = init_parameters()
+        self.assertEqual(options.source, 'fetch', "Failure to pass URL from command line")
+
+
